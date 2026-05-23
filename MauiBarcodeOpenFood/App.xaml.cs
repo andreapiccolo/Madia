2d3abc37b29be2f2c82288b09e7162e53702fd84
Andrea Piccolo <andrea@andreapiccolo.net>
2026-05-23 04:32:43 +0200

    Add .NET MAUI barcode scanner app using Open Food Facts

---
+++ MauiBarcodeOpenFood/App.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiBarcodeOpenFood;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MauiBarcodeOpenFood/MauiProgram.cs
@@ -0,0 +1,22 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using ZXing.Net.Maui;
+
+namespace MauiBarcodeOpenFood;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseBarcodeReader()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        return builder.Build();
+    }
+}
